19a06b019e77572e1daf53f4110a4e97364cdcfc
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-12-30 19:54:12 +0530

    Add files via upload

---
+++ R/HR Salary Analysis (Decison Tree Regressor)/decision_tree_regressor.R
@@ -0,0 +1,25 @@
+# Decision Tree Regression
+
+# Importing the dataset
+dataset = read.csv('HR Salary Analysis (Decison Tree Regressor)//Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Training the Decision Tree Regressor to the dataset
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting new results
+y_pred = predict(regressor, data.frame(Level = 6.5))
+print(y_pred)
+
+# Visualising the results (with high resolution)
+library(ggplot2)
+X_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+    geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+    geom_line(aes(x = X_grid, y = predict(regressor, newdata = data.frame(Level = X_grid))), colour = 'blue') +
+    ggtitle('Truth or Bluff (Decision Tree Regressor)') +
+    xlab('Level') +
+    ylab('Salary')
