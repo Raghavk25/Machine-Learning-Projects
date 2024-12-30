e84ae7327685bf2facdf52ed3778d3c66c66b890
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2024-12-30 18:38:12 +0530

    Add files via upload

---
+++ R/HR Salary Analysis (SVR)/support_vector_regression.R
@@ -0,0 +1,26 @@
+# Support Vector Regression
+
+# Importing the dataset
+dataset = read.csv('HR Salary Analysis (SVR)//Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Training the SVR model to the dataset
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+print(y_pred)
+
+# Visualising the SVR results
+library(ggplot2)
+ggplot()+
+        geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red')+
+        geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)), colour = 'blue')+
+        ggtitle('Truth or Bluff (SVR)')+
+        xlab('Level')+
+        ylab('Salary')
+
+
