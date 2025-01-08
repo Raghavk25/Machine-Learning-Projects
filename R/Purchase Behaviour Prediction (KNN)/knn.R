fe2445fe5a575e3f7bd8959f19b94f5eb081150c
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-08 13:58:17 +0530

    Update knn.R

--- R/Purchase Behaviour Prediction (KNN)/knn.R
+++ R/Purchase Behaviour Prediction (KNN)/knn.R
@@ -1,7 +1,7 @@
 # K-Nearest Neighbors Classifier
 
 # Importing the dataset
-dataset = read.csv("Purchase Behaviour Prediction//Social_Network_Ads.csv")
+dataset = read.csv("Social_Network_Ads.csv")
 
 # Splitting the dataset into Training Set and Test Set
 library(caTools)
