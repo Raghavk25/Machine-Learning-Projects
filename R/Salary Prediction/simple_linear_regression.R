a2e12bfcfcd9ce0f583e27259776ababae60c206
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:56:08 +0530

    Update simple_linear_regression.R

--- R/Salary Prediction/simple_linear_regression.R
+++ R/Salary Prediction/simple_linear_regression.R
@@ -1,24 +1,24 @@
 # Simple Linear Regression
 
 # Importing the datasets
-dataset = read.csv('Salary_Data.csv')
+dataset <- read.csv('Salary_Data.csv')
 
 # Splitting the dataset into Training set and Test set
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Salary, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split <- sample.split(dataset$Salary, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
 
 # Fitting Simple Linear Regression to the Training set
-regressor = lm(formula = Salary ~ YearsExperience, 
+regressor <- lm(formula = Salary ~ YearsExperience, 
                data = training_set)
 
 # Description of the regressor
 print(summary(regressor))
 
 # Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set)
+y_pred <- predict(regressor, newdata = test_set)
 
 # Visualizing the Training set results
 library(ggplot2)
