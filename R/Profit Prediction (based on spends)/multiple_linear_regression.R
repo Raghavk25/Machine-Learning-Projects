749d5ac3cf29b919f3da7d2603b80a9ee9635600
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:49:08 +0530

    Update multiple_linear_regression.R

--- R/Profit Prediction (based on spends)/multiple_linear_regression.R
+++ R/Profit Prediction (based on spends)/multiple_linear_regression.R
@@ -1,53 +1,53 @@
 # Multiple Linear Regression
 
 # Importing datasets
-dataset = read.csv('Profit Prediction (based on spends)//50_Startups.csv')
+dataset <- read.csv('Profit Prediction (based on spends)//50_Startups.csv')
 
 # Encoding categorical variable
-dataset$State = factor(dataset$State,
+dataset$State <- factor(dataset$State,
                        levels = c('New York', 'California', 'Florida'),
                        labels = c(1, 2, 3))
 
 # Splitting the dataset into Training set and Test set
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Profit, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split <- sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
 
 # Fitting Multiple Linear Regression to Training set
-regressor = lm(formula = Profit ~ .,
+regressor <- lm(formula = Profit ~ .,
                data = training_set)
 
 # Description of our regressor
 print(summary(regressor))
 
 # Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set)
+y_pred <- predict(regressor, newdata = test_set)
 
 # Building an optimal model through Backward Elimination
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + 
+regressor <- lm(formula = Profit ~ R.D.Spend + Administration + 
                Marketing.Spend +  State,
                data = dataset)
 print(summary(regressor))
 
 # Now, since the State variable has the highest P-value that is also above significance level (0.05), we eliminate it.
 # Then we rebuild our model without the State variable.
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + 
+regressor <- lm(formula = Profit ~ R.D.Spend + Administration + 
                Marketing.Spend,
                data = dataset)
 print(summary(regressor))
 
 # Now, we remove the next variable with the highest P-value i.e. Administration
 # Then we rebuild our model without the Administration variable.
-regressor = lm(formula = Profit ~ R.D.Spend + 
+regressor <- lm(formula = Profit ~ R.D.Spend + 
                Marketing.Spend,
                data = dataset)
 print(summary(regressor))
 
 # Now, we remove the next variable with the highest P-value i.e. Marketing Spend
 # Then we rebuild our model without the Marketing Spend variable.
-regressor = lm(formula = Profit ~ R.D.Spend,
+regressor <- lm(formula = Profit ~ R.D.Spend,
                data = dataset)
 print(summary(regressor))
 
