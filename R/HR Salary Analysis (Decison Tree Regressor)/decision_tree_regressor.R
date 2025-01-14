3d322f3714d724f05f8a93792d83c6d6a0bcf5e5
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:46:06 +0530

    Update decision_tree_regressor.R

--- R/HR Salary Analysis (Decison Tree Regressor)/decision_tree_regressor.R
+++ R/HR Salary Analysis (Decison Tree Regressor)/decision_tree_regressor.R
@@ -1,22 +1,22 @@
 # Decision Tree Regression
 
 # Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+dataset <- read.csv('Position_Salaries.csv')
+dataset <- dataset[2:3]
 
 # Training the Decision Tree Regressor to the dataset
 library(rpart)
-regressor = rpart(formula = Salary ~ .,
+regressor <- rpart(formula = Salary ~ .,
                   data = dataset,
                   control = rpart.control(minsplit = 1))
 
 # Predicting new results
-y_pred = predict(regressor, data.frame(Level = 6.5))
+y_pred <- predict(regressor, data.frame(Level = 6.5))
 print(y_pred)
 
 # Visualising the results (with high resolution)
 library(ggplot2)
-X_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+X_grid <- seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
     geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
     geom_line(aes(x = X_grid, y = predict(regressor, newdata = data.frame(Level = X_grid))), colour = 'blue') +
