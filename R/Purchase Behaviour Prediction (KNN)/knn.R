5b99dc1f856f0ba555992422a109b7237766a0d8
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:50:52 +0530

    Update knn.R

--- R/Purchase Behaviour Prediction (KNN)/knn.R
+++ R/Purchase Behaviour Prediction (KNN)/knn.R
@@ -1,38 +1,38 @@
 # K-Nearest Neighbors Classifier
 
 # Importing the dataset
-dataset = read.csv("Social_Network_Ads.csv")
+dataset <- read.csv("Social_Network_Ads.csv")
 
 # Splitting the dataset into Training Set and Test Set
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
 
 # Feature scaling
-training_set[, 1:2] = scale(training_set[, 1:2])
-test_set[, 1:2] = scale(test_set[, 1:2])
+training_set[, 1:2] <- scale(training_set[, 1:2])
+test_set[, 1:2] <- scale(test_set[, 1:2])
 
 # Fitting the KNN Classifier to the Training Set and predicting the Test Set results
 library(class)
-y_pred = knn(train = training_set[, -3],
+y_pred <- knn(train = training_set[, -3],
              test = test_set[, -3],
              cl = training_set[, 3],
              k = 5)
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred)
+cm <- table(test_set[, 3], y_pred)
 cm
 
 # Visualising the Training Set results
 library(ElemStatLearn)
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[, -3],
+set <- training_set
+X1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(X1, X2)
+colnames(grid_set) <- c('Age', 'EstimatedSalary')
+y_grid <- knn(train = training_set[, -3],
              test = grid_set,
              cl = training_set[, 3],
              k = 5)
@@ -46,12 +46,12 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
 
 # Visualising the Test Set results
 library(ElemStatLearn)
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[, -3],
+set <- test_set
+X1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(X1, X2)
+colnames(grid_set) <- c('Age', 'EstimatedSalary')
+y_grid <- knn(train = training_set[, -3],
              test = grid_set,
              cl = training_set[, 3],
              k = 5)
