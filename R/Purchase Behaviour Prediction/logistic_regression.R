e2ee4bece81edee89960a2ee7c37f194bad2844f
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:55:27 +0530

    Update logistic_regression.R

--- R/Purchase Behaviour Prediction/logistic_regression.R
+++ R/Purchase Behaviour Prediction/logistic_regression.R
@@ -1,42 +1,42 @@
 # Logistic Regression
 
 # Importing the dataset
-dataset = read.csv("Social_Network_Ads.csv")
+dataset <- read.csv("Social_Network_Ads.csv")
 
 # Splitting the dataset into Training Set and Test Set
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
 
 # Feature scaling
-training_set[, 1:2] = scale(training_set[, 1:2])
-test_set[, 1:2] = scale(test_set[, 1:2])
+training_set[, 1:2] <- scale(training_set[, 1:2])
+test_set[, 1:2] <- scale(test_set[, 1:2])
 
 # Fitting the Logistic Regression to the Training Set
-classifier = glm(formula = Purchased ~ .,
+classifier <- glm(formula = Purchased ~ .,
                  family = binomial,
                  data = training_set)
 
 # Predicting new results
-prob_pred = predict(classifier, type = "response", newdata = test_set[-3])
-y_pred = ifelse(prob_pred >= 0.5, 1, 0)
+prob_pred <- predict(classifier, type = "response", newdata = test_set[-3])
+y_pred <- ifelse(prob_pred >= 0.5, 1, 0)
 y_pred
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred)
+cm <- table(test_set[, 3], y_pred)
 cm
 
 # Visualising the Training Set results
 library(ElemStatLearn)
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+set <- training_set
+X1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(X1, X2)
+colnames(grid_set) <- c('Age', 'EstimatedSalary')
+prob_set <- predict(classifier, type = 'response', newdata = grid_set)
+y_grid <- ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
      main = 'Logistic Regression (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
@@ -47,13 +47,13 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
 
 # Visualising the Test Set results
 library(ElemStatLearn)
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+set <- test_set
+X1 <- seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 <- seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set <- expand.grid(X1, X2)
+colnames(grid_set) <- c('Age', 'EstimatedSalary')
+prob_set <- predict(classifier, type = 'response', newdata = grid_set)
+y_grid <- ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
      main = 'Logistic Regression (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
