3bc3a913d8b01221e0b4e1bc23910c43f6bf8121
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-01-14 19:46:43 +0530

    Update random_forest_regression.R

--- R/HR Salary Analysis (Random Forest Regressor)/random_forest_regression.R
+++ R/HR Salary Analysis (Random Forest Regressor)/random_forest_regression.R
@@ -1,23 +1,23 @@
 # Random Forest Regression
 
 # Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+dataset <- read.csv('Position_Salaries.csv')
+dataset <- dataset[2:3]
 
 # Training the Random Forest Regressor to the dataset
 library(randomForest)
 set.seed(1234)
-regressor = randomForest(x = dataset[1], 
+regressor <- randomForest(x = dataset[1], 
                          y = dataset$Salary,
                          ntree = 1000)
 
 # Predicting new results
-y_pred = predict(regressor, newdata = data.frame(Level = 6.5))
+y_pred <- predict(regressor, newdata = data.frame(Level = 6.5))
 print(y_pred)
 
 # Visualising results of the regressor
 library(ggplot2)
-X_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+X_grid <- seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
     geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
     geom_line(aes(x = X_grid, y = predict(regressor, newdata = data.frame(Level = X_grid))), colour = 'blue') +
