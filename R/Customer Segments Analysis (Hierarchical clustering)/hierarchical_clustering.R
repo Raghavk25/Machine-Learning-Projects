3494eac146eac2d02dbd7060334095c68d0b5aaa
Raghav Khanna <112757016+Raghavk25@users.noreply.github.com>
2025-02-06 20:17:38 +0530

    Add files via upload

---
+++ R/Customer Segments Analysis (Hierarchical clustering)/hierarchical_clustering.R
@@ -0,0 +1,30 @@
+# Hierarchical clustering
+
+# Importing the dataset
+dataset <- read.csv("Customer Segments Analysis (Hierarchical clustering)//Mall_Customers.csv")
+dataset <- dataset[, 4:5]
+
+# Using the dendrogram to find the optimal number of clusters
+dendrogram <- hclust(dist(dataset, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = 'Dendrogram',
+     xlab = 'Customers',
+     ylab = 'Euclidean distances')
+
+# Fitting the hierarchical clustering algorithm to the dataset
+hc <- hclust(dist(dataset, method = 'euclidean'), method = 'ward.D')
+y_hc <- cutree(hc, 5)
+
+# Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_hc,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = "Clusters of clients",
+         xlab = "Annual Income (k$)",
+         ylab = "Spending Score (1-100)")
